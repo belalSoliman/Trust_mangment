e430179643e140afb777098f43628ccd73848266
belal_soliman <belalsoliman8@gmail.com>
2025-02-09 19:19:21 +0200

    Add splash and app icon images for various resolutions

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
-void main() {
+void main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  // Your initialization code here
+
+  // Remove splash screen when initialization is complete
+  FlutterNativeSplash.remove();
   runApp(const MyApp());
 }
 
